839be0545682257df852b5b767fc8ffa1d399256
jingjin412 <b10204022@g.ntu.edu.tw>
2024-07-29 00:32:19 +0800

    merge dataset

--- Pointwise Reranking Phase/run.sh
+++ Pointwise Reranking Phase/run.sh
@@ -1,10 +1,10 @@
 python Llama_inference.py \
-        data/reading_comprehension_data_llama_1000.json \
-        data/reading_comprehension_data_openai_1000.json \
+        ../data/reading_comprehension_data_llama_1000.json \
+        ../data/reading_comprehension_data_openai_1000.json \
         0 0 \
         results/score_llama_normal.txt
 python ChatGPT_inference.py \
-        data/reading_comprehension_data_llama_1000.json \
-        data/reading_comprehension_data_openai_1000.json \
+        ../data/reading_comprehension_data_llama_1000.json \
+        ../data/reading_comprehension_data_openai_1000.json \
         0 0 \
         results/score_chatgpt_normal.txt
