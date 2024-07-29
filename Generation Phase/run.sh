407bf723e32f5e941183000a1b216937c5c82e7c
Lily <yenshanchen.tw@gmail.com>
2024-07-28 22:44:56 -0700

    [UPD] generation phase data path

--- Generation Phase/run.sh
+++ Generation Phase/run.sh
@@ -1,13 +1,13 @@
 python Llama_inference.py \
-        data/reading_comprehension_data_llama_1000.json \
-        data/reading_comprehension_data_openai_1000.json \
+        ../data/reading_comprehension_data_llama_1000.json \
+        ../data/reading_comprehension_data_openai_1000.json \
         results/0721_llama_vs_chatgpt/ \
         results/0721_llama_vs_human/ \
         2 2 \
         > results/0721_llama_results.txt
 python ChatGPT_inference.py \
-        data/reading_comprehension_data_llama_1000.json \
-        data/reading_comprehension_data_openai_1000.json \
+        ../data/reading_comprehension_data_llama_1000.json \
+        ../data/reading_comprehension_data_openai_1000.json \
         results/0721_chatgpt_vs_llama/ \
         results/0721_chatgpt_vs_human/ \
         2 2 \
